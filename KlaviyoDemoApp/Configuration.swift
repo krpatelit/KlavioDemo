95e2f4bf4b99009ad6fc1c03ba43bf525cf83983
Kiran Patel <kiran.patel@mavenir.com>
2022-08-11 16:23:07 -0700

    Updated file structure

---
+++ KlaviyoDemoApp/Configuration.swift
@@ -0,0 +1,15 @@
+//
+//  GlobalVariables.swift
+//  KlaviyoDemoApp
+//
+//  Created by Kiran Patel on 8/10/22.
+//
+
+import Foundation
+
+//This is for demo purpose only
+class Global {
+    static var user = User()
+    static let apiKey = "pk_96b0fe8936212b7aa32e67897b086660e8"
+    static let defaultEmailId = "kiran@klaviyo.com"
+}
--- KlaviyoDemoApp/Utils/UserData.swift
+++ KlaviyoDemoApp/Utils/UserData.swift
@@ -1,21 +1,15 @@
 //
-//  GlobalVariables.swift
+//  UserData.swift
 //  KlaviyoDemoApp
 //
-//  Created by Kiran Patel on 8/10/22.
+//  Created by Kiran Patel on 8/11/22.
 //
 
 import Foundation
 
-//This is for demo purpose only
-class Global {
-    static var user = User()
-    static let apiKey = "pk_96b0fe8936212b7aa32e67897b086660e8"
-}
-
 //This is only for demo purpose, Ideally data should be coming from DB
 class User {
-    var emailId: String = "kiran@klaviyo.com" {
+    var emailId: String = Global.defaultEmailId {
         didSet {
             if emailId != oldValue {
                 self.updateToDB()
